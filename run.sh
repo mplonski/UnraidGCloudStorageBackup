b04f7af5a5fb44656a44328f18fcf625a7deca0b
Maciej Plonski <mplonski@users.noreply.github.com>
2024-05-13 03:34:46 +0200

    remove not working echo

--- run.sh
+++ run.sh
@@ -60,8 +60,7 @@ elif [[ $OPTION = "backup" ]]; then
     touch $LOCKFILE
   fi
 
-  log_info "Executing: gsutil -m rsync -r /data/backup $GCSPATH with options:"
-  echo $GCSOPTIONS
+  log_info "Executing: gsutil -m rsync -r /data/backup $GCSPATH"
   CLOUDSDK_PYTHON="python3" sh /google-cloud-sdk/bin/gsutil -m rsync -r $GCSOPTIONS /data/backup $GCSPATH >> $LOG 2>&1
   rm -f $LOCKFILE
   log_info "Finished sync: $(date)"
